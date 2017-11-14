3882e18ffc42970a04f1e6eb4be0e58227095499
ml1150258 <mark.lehr@rcc.edu>
2017-11-14 12:09:49 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

--- Lab/Home_V1_Computations/main.cpp
+++ Lab/Home_V1_Computations/main.cpp
@@ -1,8 +1,8 @@
 /* 
  * File:   main.cpp
  * Author: Dr. Mark E. Lehr
- * Created on November 9th, 2017, 12:15 PM
- * Purpose:  Retirement Calculator Version 1
+ * Created on November 14th, 2017, 11:25 AM
+ * Purpose:  Home Calculator Version 1
  */
 
 //System Libraries
@@ -13,32 +13,44 @@ using namespace std;    //Standard Name-space under which System Libraries resid
 //User Libraries
 
 //Global Constants - Not variables only Math/Science/Conversion constants
+const int CNVMNYR=12; //Conversion Months to Year
+const int CNVPRCT=100;//Conversion to Percent
 
 //Function Prototypes
 
 //Execution Begins Here!
 int main(int argc, char** argv) {
     //Declare Variables
-    float salary, deposit, invRate, savings, retAcct;
+    float pPrice, pDown, loanAmt, intRate, mnPmt;
+    int nMonths;
     
     //Initialize Variables
-    salary=100000;          //$100k
-    invRate=0.05;           //5%
-    savings=salary/invRate; //Required savings to retire $'s
-    deposit=0.10;           //10%/year
-    retAcct=0;              //Initialize the retirement account
+    pPrice=350000.00f;          //Purchase Price  = $350k
+    pDown=0.20f;                //Percentage Down = 20%
+    loanAmt=pPrice*(1-pDown);   //Loan Amount $'s
+    intRate=0.05f;              //Interest Rate / Year
+    mnPmt=1503.11f;             //Monthly Payment $'s
+    nMonths=360;                //Number of months in loan payment
     
     //Process or map the inputs to the outputs
-    cout<<"Retirement Table"<<endl;
-    cout<<"Year      Savings  Int Earned    Deposit"<<endl;
     cout<<fixed<<setprecision(2)<<showpoint;
-    for(int year=2022;year<=2072;year++){
-        float intErnd=invRate*retAcct;//Interest earned for the year
-        float depAmt=deposit*salary; //Yearly deposit
-        cout<<setw(4)<<year<<setw(13)<<retAcct<<setw(12)<<intErnd
-                <<setw(11)<<depAmt<<endl;
-        retAcct+=(intErnd+depAmt);
+    cout<<"Home Amortization Table"<<endl<<endl;
+    cout<<"Purchase Price = $"<<pPrice<<endl;
+    cout<<"Percent Down   =      "<<pDown*CNVPRCT<<"%"<<endl;
+    cout<<"Loan Amount    = $"<<loanAmt<<endl;
+    cout<<"Interest Rate  =       "<<intRate*CNVPRCT<<"%"<<endl;
+    cout<<"Monthly Payment= $  "<<mnPmt<<endl<<endl;
+    cout<<"Month     Loan Amount     Int Paid    Payment"<<endl;
+    for(int month=0;month<nMonths;month++){
+        float intAccm=intRate/CNVMNYR*loanAmt;//Interest Accrued per month
+        cout<<setw(5)<<month<<setw(16)<<loanAmt<<setw(13)<<intAccm
+                <<setw(11)<<mnPmt<<endl;
+        loanAmt+=(intAccm-mnPmt);
     }
+    cout<<endl;
+    cout<<"Last Payment = $"<<(mnPmt+loanAmt)<<endl;
+    cout<<"Total Paid for Loan = $"<<nMonths*mnPmt+loanAmt<<endl;
+    cout<<"Total Paid for Home = $"<<pPrice*pDown+nMonths*mnPmt+loanAmt<<endl;
     
     //Exit the program
     return 0;
